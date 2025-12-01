ebc65f5868c48970707a874c9772943696c101f2
luwei <1261579359@qq.com>
2025-12-01 22:27:04 +0800

    update, not finished, complie error

--- src/mydb.cpp
+++ src/mydb.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <sstream>
+#include <cstdint>
 
 using namespace std;
 
 void print_prompt();
+void print_row(Row* row);
 
 typedef enum {
     META_COMMAND_SUCCESS,
@@ -10,7 +13,8 @@ typedef enum {
 } MetaCommandResult;
 
 typedef enum { 
-    PREPARE_SUCCESS, 
+    PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR, 
     PREPARE_UNRECOGNIZED_STATEMENT 
 } PrepareResult;
 
@@ -19,10 +23,49 @@ typedef enum {
     STATEMENT_SELECT 
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
+// (Struct*)0：将 0 转换为指向 Struct 类型的指针
+// ((Struct*)0)->Attribute：访问这个"假指针"指向的成员
+// sizeof(...)：计算该成员的大小
+// 这个操作在编译时完成，不产生运行时开销
+// 因为只是计算类型大小，不需要实际的内存访问
+// 编译器知道成员的类型，所以能直接计算大小
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+#define TABLE_MAX_PAGES 100
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+typedef enum { 
+    EXECUTE_SUCCESS, 
+    EXECUTE_TABLE_FULL 
+} ExecuteResult;
 
 void print_prompt() { 
     cout << "db > "; 
@@ -32,6 +75,17 @@ void print_prompt() {
 PrepareResult prepare_statement(string input_buffer, Statement* statement) {
     if (input_buffer.substr(0, 6) == "insert") {
         statement->type = STATEMENT_INSERT;
+        istringstream iss(input_buffer);
+        string command;
+
+        if (!(iss >> command >> statement->row_to_insert.id >> statement->row_to_insert.username >> statement->row_to_insert.email)) {
+            return PREPARE_SYNTAX_ERROR;
+        }
+
+        if (command != "insert") {
+            return PREPARE_SYNTAX_ERROR;
+        }
+
         return PREPARE_SUCCESS;
     }
     if (input_buffer == "select") {
@@ -41,17 +95,115 @@ PrepareResult prepare_statement(string input_buffer, Statement* statement) {
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+        case (STATEMENT_INSERT):
+            printf("This is where we would do an insert.\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("This is where we would do a select.\n");
+            break;
+    }
+}
+
+MetaCommandResult do_meta_command(string input_buffer) {
+  if (input_buffer == ".exit") {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+// 找到特定的行
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (page == NULL) {
+        // Allocate memory only when we try to access page
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+    Row* row_to_insert = &(statement->row_to_insert);
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+void print_row(Row* row) {
+    cout << "(" << row->id << ", " << row->username << ", " << row->email << ")" << endl;
+}
+
+
+Table* new_table() {
+    Table* table = (Table*)malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table* table) {
+    for (int i = 0; table->pages[i]; i++) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
 int main(int argc, char* argv[]) {
     string input_buffer;
 
     while (true) {
         print_prompt();
         getline(cin, input_buffer);
-        if (input_buffer == ".exit") {
-            cout << "Bye!" << endl;
-            exit(EXIT_SUCCESS);
-        } else {
-          cout << "Unrecognized command '" << input_buffer << "'." << endl;
+        if (!input_buffer.empty() && input_buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    cout << "Unrecognized command '" << input_buffer << "'." << endl;
+                    continue;
+            }
         }
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                cout << "Unrecognized keyword at start of '" << input_buffer << "'." << endl;
+                continue;
+        }
+        execute_statement(&statement);
+        cout << "Executed." << endl;
     }
 }
