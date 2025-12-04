172c0da7ae1cf5e8a8a9724e3903f4b3c6c00858
luwei <1261579359@qq.com>
2025-12-04 22:27:46 +0800

    向B树切换，数据没有持久化

--- include/mydb.h
+++ include/mydb.h
@@ -60,16 +60,16 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
-
 typedef struct {
-  int file_descriptor;
-  uint32_t file_length;
-  void* pages[TABLE_MAX_PAGES];
+    int file_descriptor;
+    uint32_t file_length;
+    uint32_t num_pages;
+    void* pages[TABLE_MAX_PAGES];
 } Pager;
 
 typedef struct {
-    uint32_t num_rows;
     Pager* pager;
+    uint32_t root_page_num;
 } Table;
 
 typedef enum { 
@@ -79,11 +79,44 @@ typedef enum {
 } ExecuteResult;
 
 typedef struct {
-    Table* table;
-    uint32_t row_num;
-    bool end_of_table;  // Indicates a position one past the last element
+  Table* table;
+  uint32_t page_num;
+  uint32_t cell_num;
+  bool end_of_table;  // Indicates a position one past the last element
 } Cursor;
 
+typedef enum { 
+    NODE_INTERNAL, 
+    NODE_LEAF 
+} NodeType;
+
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+
+/*
+ * Leaf Node Header Layout
+ */
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+
+/*
+ * Leaf Node Body Layout
+ */
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 void print_prompt();
 PrepareResult prepare_statement(std::string input_buffer, Statement* statement);
@@ -99,9 +132,17 @@ Pager* pager_open(const char* filename);
 Table* db_open(const char* filename);
 void* get_page(Pager* pager, uint32_t page_num);
 void db_close(Table* table);
-void pager_flush(Pager* pager, uint32_t page_num, uint32_t size);
+void pager_flush(Pager* pager, uint32_t page_num);
 Cursor* table_start(Table* table);
 Cursor* table_end(Table* table);
 void cursor_advance(Cursor* cursor);
+void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value);
+void print_constants();
+void print_leaf_node(void* node);
+uint32_t* leaf_node_num_cells(void* node);
+void* leaf_node_cell(void* node, uint32_t cell_num);
+uint32_t* leaf_node_key(void* node, uint32_t cell_num);
+void* leaf_node_value(void* node, uint32_t cell_num);
+void initialize_leaf_node(void* node);
 
 #endif
